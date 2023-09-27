a4e99b27ba33f0f8a2a2013fc7327465f05e2337
caro-turnbull <caro.turnbull@gmail.com>
2023-09-27 09:13:03 -0400

    user model and spec set-up

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'should require a first name' do
+    end
+    it 'should require a last name' do
+    end
+    it 'should require an email' do
+    end
+    it 'should have unique case-sensitive email' do
+    end
+    it 'should require a password' do
+    end
+    it 'should require a password confimration' do
+    end
+    it 'should having matching password and confirmation' do
+    end
+
+  end
+
+end
