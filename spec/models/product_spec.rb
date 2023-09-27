898372fd77721cc7629ef57bf7a82f2e6abb26a9
caro-turnbull <caro.turnbull@gmail.com>
2023-09-26 20:40:19 -0400

    rspec validations

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'should validate name' do
+      @category= Category.new(name: "categoryName")
+      @product = Product.new(name: nil, price: 200, quantity: "1", category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include ("Name can't be blank")
+      puts @product.errors.full_messages
+    end
+    it 'should validate price' do
+      @category= Category.new(name: "categoryName")
+      @product = Product.new(name: "testName", price: , quantity: "1", category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include ("Price can't be blank")
+    end
+    it 'should validate quantity' do
+      @category= Category.new(name: "categoryName")
+      @product = Product.new(name: "testName", price: 200, quantity: nil, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include ("Quantity can't be blank")
+    end
+    it 'should validate category' do
+      @category= Category.new(name: "categoryName")
+      @product = Product.new(name: "testName", price: 200, quantity: 3, category: nil)
+      @product.save
+      expect(@product.errors.full_messages).to include ("Category can't be blank")
+    end
+  end
+end
